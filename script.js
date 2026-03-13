8c2fdd6ce30395cdd7d34a0b58bea84476c539ee
Hemadharshini <darshinihema2102@gmail.com>
2026-03-13 23:02:55 +0530

    script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ btn.disabled = true;
 btn.innerText = "Sending...";
 
 const params = {
-name: this.name.value,
-phone: this.phone.value,
-email: this.email.value,
-message: this.message.value
+name: document.querySelector('input[name="name"]').value,
+phone: document.querySelector('input[name="phone"]').value,
+email: document.querySelector('input[name="email"]').value,
+message: document.querySelector('textarea[name="message"]').value
 };
 
 emailjs.send(
@@ -37,12 +37,12 @@ btn.innerText = "✉ Send Message";
 
 .catch(function(error){
 
-console.error("EmailJS Error:", error);
+console.log("EmailJS FULL ERROR:", error);
 
 btn.disabled = false;
 btn.innerText = "✉ Send Message";
 
-alert("Message failed. Please try again.");
+alert("Message failed. Check console.");
 
 });
 
