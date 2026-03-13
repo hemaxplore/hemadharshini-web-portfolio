e25b66d18bebe3cca28ecc52a3a5b824e3e724a6
Hemadharshini <darshinihema2102@gmail.com>
2026-03-13 20:41:35 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Modern Portfolio Loaded");
+
+function showToast(){
+
+    const toast = document.getElementById("toast");
+
+    toast.classList.add("show");
+
+    setTimeout(() => {
+        toast.classList.remove("show");
+    }, 3500);
+
+}
+
